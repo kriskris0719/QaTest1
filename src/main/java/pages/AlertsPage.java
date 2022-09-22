13fe99a84a2f988b84ad940e0fc5a037eda21e3a
Khrystyna Zhelo <kriart0719@gmail.com>
2022-09-22 17:52:56 +0300

    Updates

--- src/main/java/pages/AlertsPage.java
+++ src/main/java/pages/AlertsPage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 public class AlertsPage {
     private WebDriver driver;
     private By triggerAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
+    private By triggerConfirmButton = By.xpath(".//button[text()='Click for JS Confirm']");
+    private By triggerPromptButton = By.xpath(".//button[text()='Click for JS Prompt']");
+    private By results = By.id("result");
 
     public AlertsPage(WebDriver driver) {
         this.driver = driver;
@@ -13,7 +16,31 @@ public class AlertsPage {
     public void triggerAlert(){
         driver.findElement(triggerAlertButton).click();
     }
-    public void acceptAlert(){
+
+    public void triggerConfirm(){
+        driver.findElement(triggerConfirmButton).click();
+    }
+
+    public void triggerPrompt(){
+        driver.findElement(triggerPromptButton).click();
+    }
+
+    public void alert_clickToAccept(){
         driver.switchTo().alert().accept();
+    }
+    public void alert_clickToDismiss(){
+        driver.switchTo().alert().dismiss();
+    }
+
+    public String alert_getText(){
+        return driver.switchTo().alert().getText();
+    }
+
+    public void alert_setInput(String text){
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    public String getResult(){
+        return driver.findElement(results).getText();
     }
 }
