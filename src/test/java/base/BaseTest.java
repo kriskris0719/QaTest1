e2d2b854eb40ce39ab28107c5dec03b8c2309a96
Khrystyna Zhelo <kriart0719@gmail.com>
2022-09-27 16:41:13 +0300

    frtghuj

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,18 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -66,6 +72,21 @@ public class BaseTest {
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
+    }
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+
+        if(ITestResult.FAILURE == result.getStatus())
+        {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+
+            try{
+                Files.move(screenshot, new File("C:\\GIT\\QaTest1\\src\\test\\resources\\screenshots\\" + result.getName() + ".png"));
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public WindowManager getWindowManager(){
