ccd1346190076fad2173c2fc7d4835a5b76c2420
Khrystyna Zhelo <kriart0719@gmail.com>
2022-09-27 17:31:07 +0300

    fghj

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
@@ -17,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    private WebDriver driver;
+    private EventFiringWebDriver driver;;
 
     protected HomePage homePage;
     protected KeyPressesPage keyPressesPage;
@@ -43,7 +45,8 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         driver.manage().window().maximize();
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://the-internet.herokuapp.com/");
